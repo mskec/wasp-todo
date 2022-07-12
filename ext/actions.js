2311af376ec65b7c7c424a272ff6679d8cd84f05
mskec <martin.skec@gmail.com>
2022-07-12 09:51:29 +0200

    Adding auth

--- ext/actions.js
+++ ext/actions.js
@@ -1,12 +1,23 @@
+import HttpError from '@wasp/core/HttpError.js';
+
 export const createTask = async (args, context) => {
+	if (!context.user) {
+		throw new HttpError(403);
+	}
 	return context.entities.Task.create({
-		data: { description: args.description },
+		data: {
+			description: args.description,
+			user: { connect: { id: context.user.id } },
+		},
 	});
 };
 
 export const updateTask = async (args, context) => {
-	return context.entities.Task.update({
-		where: { id: args.id },
+	if (!context.user) {
+		throw new HttpError(403);
+	}
+	return context.entities.Task.updateMany({
+		where: { id: args.id, user: { id: context.user.id } },
 		data: { isDone: args.isDone },
 	});
 };
